52142f07966d44fc5a7bbd983b2a8ecd1253efc5
QiQ <74490611+sunqihao77@users.noreply.github.com>
2023-04-05 13:49:22 +0800

    change routes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Ace Prompting',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -92,7 +92,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/docs/Tutorials',
               },
             ],
           },
